b1ad1948f0b0d9ba56959ad709f7a7bd1e4aea75
howard <howard@kakao.com>
2014-03-21 00:29:04 +0900

    payment, category table added

---
+++ src/Pisa/Pisa/DB/PaymentTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pisa.DB
+{
+	[Table]
+	public class PaymentTable
+	{
+	}
+}
--- src/Pisa/Pisa/Model/PisaModel.cs
+++ src/Pisa/Pisa/Model/PisaModel.cs
@@ -7,9 +7,36 @@ using System.Threading.Tasks;
 
 namespace Pisa.Model
 {
-	public class PisaModel : INotifyPropertyChanged
+	public enum PaymentType
+	{
+		None = 0,
+		Cache = 1,
+		Card = 2,
+		BankBook = 3
+	}
+
+	public enum Category
 	{
 
+	}
+
+	public class PisaModel : INotifyPropertyChanged
+	{
+		#region DateTime Date
+		private DateTime _Date;
+		public DateTime Date
+		{
+			get
+			{
+				return _Date;
+			}
+			set
+			{
+				_Date = value;
+				OnPropertyChanged("Date");
+			}
+		}
+		#endregion DateTime Date
 
 
 		#region ▶ INotifyPropertyChanged
